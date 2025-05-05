8af7bec525ee917c51d11c2aaeaf12afdd42f1d5
IsaiasDesky <isaias@desky.la>
2025-05-05 06:41:43 -0300

    added suspense component for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Collections } from "@/UI/sections/home/Collections";
 import { TransactionHistory } from "@/UI/sections/home/TransactionHistory";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <Collections />
-      <TransactionHistory />
+      <Suspense fallback={null}>
+        <TransactionHistory />
+      </Suspense>
     </main>
   );
 }
